9587f2cbb9a25a1763fcb4ac56f4c4f1823f16e7
murthy ganti <murthyganti8885@gmail.com>
2021-08-01 09:00:49 +0530

    fuck

---
+++ fuck/arithmetic operators.py
@@ -0,0 +1,34 @@
+"""
+arithemettic operations
++ - addittion
+- - substraction
+/ - floating pt division
+* - multiplication
+// - integer division
+% - modules (it gives reminder)
+** - Exponentiation operators
+== - Comparission of equality
+> - Greatr Than
+>= - Greater Than or equal
+< - Lesser Than
+<= - Lesser Than or equal
+"""
+
+a = 5
+b = 2
+c = 5
+
+print ("Arithematic operations")
+print (a+b) # 7
+print (a-b) # 3
+print (a*b) # 10
+print (a/b) # 2.5
+print (a//b) #  2
+print (a%b) # 1
+print (a**b) #25
+print ("Boolean ops")
+print (a==c) # a is 5 and c is 5
+print (a>b) # a is 5 and b is 2
+print (a>=b) # a is 5 and b is 2
+print (a<b) # a is 5 and b is 2
+print (a<=b) # a is 5 and b is 2
